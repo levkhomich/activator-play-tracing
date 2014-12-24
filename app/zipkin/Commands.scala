cbed8baa84e041fa6909411fc866e19de3459310
Lev Khomich <levkhomich@gmail.com>
2014-12-24 23:28:13 +0500

    fix zipkin.sh executable flag

--- app/zipkin/Commands.scala
+++ app/zipkin/Commands.scala
@@ -1,11 +1,15 @@
 package zipkin
 
+import java.io.File
+
 import scala.sys.process._
 
 object Start extends App {
+  new File("./project/zipkin.sh").setExecutable(true)
   "./project/zipkin.sh start".run()
 }
 
 object Stop extends App {
+  new File("./project/zipkin.sh").setExecutable(true)
   "./project/zipkin.sh stop".!
 }
